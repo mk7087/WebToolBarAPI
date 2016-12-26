d6f6d028a6dd6b0aa8ba3501268e9952dd4cac24
mk7087 <yamk4054@outlook.com>
2016-12-26 17:06:52 +0900

    Added toolbars container

--- WebToolBarAPI.js
+++ WebToolBarAPI.js
@@ -2,3 +2,17 @@ var webToolBarAPI = new Object();
 webToolBarAPI.versionType = 'alpha';
 webToolBarAPI.version = 1.0;
 webToolBarAPI.isMinimum = false;
+
+function toolBarsContainerFunc(){
+  var toolBarsContainer = document.createElement('div');
+  toolBarsContainer.id = 'tbContainer';
+  document.getElementsByTagName('body')[0].appendChild(toolBarsContainer);
+}
+
+try {
+  window.addEventListener("load", toolBarsContainerFunc, false); 
+} catch (e) { 
+  //If InternetExplorer 
+  window.attachEvent("onload", toolBarsContainerFunc); 
+}
+var ToolBars = new Array(0);
