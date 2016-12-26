9940406a763ae5d69939661e45e2a97e583d84df
mk7087 <yamk4054@outlook.com>
2016-12-26 19:11:41 +0900

    Chenged function name "toolBarsContainerFunc" to "initToolBarsContainer"

--- WebToolBarAPI.js
+++ WebToolBarAPI.js
@@ -3,14 +3,14 @@ webToolBarAPI.versionType = 'alpha';
 webToolBarAPI.version = 1.0;
 webToolBarAPI.isMinimum = false;
 
-function toolBarsContainerFunc() {
+function initToolBarsContainer() {
   var toolBarsContainer = document.createElement('div');
   toolBarsContainer.id = 'tbContainer';
   document.body.appendChild(toolBarsContainer);
 }
 
 try {
-  window.addEventListener('DOMContentLoaded', toolBarsContainerFunc, false); 
+  window.addEventListener('DOMContentLoaded', initToolBarsContainer, false); 
 } catch (e) { 
   //If InternetExplorer 
   window.attachEvent('onload', toolBarsContainerFunc); 
